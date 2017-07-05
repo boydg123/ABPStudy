ecb3ba8acef0f9b43ec71285fc7af0a28ae219c3
Dou <550591292@qq.com>
2017-07-05 18:07:20 +0800

    ABP Zero缓存，会话注释

--- zeromodule/Abp.Zero/Runtime/Caching/AbpZeroCacheManagerExtensions.cs
+++ zeromodule/Abp.Zero/Runtime/Caching/AbpZeroCacheManagerExtensions.cs
@@ -5,23 +5,43 @@ using Abp.MultiTenancy;
 
 namespace Abp.Runtime.Caching
 {
+    /// <summary>
+    /// ABP Zero缓存管理扩展
+    /// </summary>
     public static class AbpZeroCacheManagerExtensions
     {
+        /// <summary>
+        /// 获取用户权限缓存
+        /// </summary>
+        /// <param name="cacheManager">缓存管理对象</param>
+        /// <returns></returns>
         public static ITypedCache<string, UserPermissionCacheItem> GetUserPermissionCache(this ICacheManager cacheManager)
         {
             return cacheManager.GetCache<string, UserPermissionCacheItem>(UserPermissionCacheItem.CacheStoreName);
         }
-
+        /// <summary>
+        /// 获取角色权限缓存
+        /// </summary>
+        /// <param name="cacheManager">缓存管理对象</param>
+        /// <returns></returns>
         public static ITypedCache<string, RolePermissionCacheItem> GetRolePermissionCache(this ICacheManager cacheManager)
         {
             return cacheManager.GetCache<string, RolePermissionCacheItem>(RolePermissionCacheItem.CacheStoreName);
         }
-
+        /// <summary>
+        /// 获取商户功能缓存
+        /// </summary>
+        /// <param name="cacheManager">缓存管理对象</param>
+        /// <returns></returns>
         public static ITypedCache<int, TenantFeatureCacheItem> GetTenantFeatureCache(this ICacheManager cacheManager)
         {
             return cacheManager.GetCache<int, TenantFeatureCacheItem>(TenantFeatureCacheItem.CacheStoreName);
         }
-
+        /// <summary>
+        /// 获取版本功能缓存
+        /// </summary>
+        /// <param name="cacheManager">缓存管理对象</param>
+        /// <returns></returns>
         public static ITypedCache<int, EditionfeatureCacheItem> GetEditionFeatureCache(this ICacheManager cacheManager)
         {
             return cacheManager.GetCache<int, EditionfeatureCacheItem>(EditionfeatureCacheItem.CacheStoreName);
--- zeromodule/Abp.Zero/Runtime/Session/AbpSessionExtensions.cs
+++ zeromodule/Abp.Zero/Runtime/Session/AbpSessionExtensions.cs
@@ -3,8 +3,17 @@ using Abp.Authorization.Users;
 
 namespace Abp.Runtime.Session
 {
+    /// <summary>
+    /// ABP Session扩展
+    /// </summary>
     public static class AbpSessionExtensions
     {
+        /// <summary>
+        /// 是否是用户
+        /// </summary>
+        /// <param name="session">ABP Session对象</param>
+        /// <param name="user">ABP 用户</param>
+        /// <returns></returns>
         public static bool IsUser(this IAbpSession session, AbpUserBase user)
         {
             if (session == null)
