1a72a770669def80db1338958f7995bbbd570030
Derrick <550591292@qq.com>
2017-07-15 22:23:22 +0800

    Core Debug翻译

--- zero/DerrickCore/Debugging/DebugHelper.cs
+++ zero/DerrickCore/Debugging/DebugHelper.cs
@@ -1,7 +1,13 @@
 ﻿namespace Derrick.Debugging
 {
+    /// <summary>
+    /// Debug帮助累
+    /// </summary>
     public static class DebugHelper
     {
+        /// <summary>
+        /// 是否是Debug
+        /// </summary>
         public static bool IsDebug
         {
             get
