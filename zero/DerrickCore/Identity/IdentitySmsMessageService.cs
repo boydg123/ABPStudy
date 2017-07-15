78d9469474430155e8db2a6b17717ce846ca597d
Derrick <550591292@qq.com>
2017-07-15 23:17:15 +0800

    Core 短信翻译

--- zero/DerrickCore/Identity/IdentitySmsMessageService.cs
+++ zero/DerrickCore/Identity/IdentitySmsMessageService.cs
@@ -5,15 +5,29 @@ using Microsoft.AspNet.Identity;
 
 namespace Derrick.Identity
 {
+    /// <summary>
+    /// 身份短信服务
+    /// </summary>
     public class IdentitySmsMessageService : IIdentityMessageService, ITransientDependency
     {
+        /// <summary>
+        /// 日志引用
+        /// </summary>
         public ILogger Logger { get; set; }
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public IdentitySmsMessageService()
         {
             Logger = NullLogger.Instance;
         }
 
+        /// <summary>
+        /// 发送身份消息
+        /// </summary>
+        /// <param name="message">身份消息</param>
+        /// <returns></returns>
         public Task SendAsync(IdentityMessage message)
         {
             //TODO: Implement this service to send SMS to users. This is used by UserManager (ASP.NET Identity) on two factor auth.
