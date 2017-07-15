c251fdb3139531e0a14e7ba89a3a5641b75497e8
Derrick <550591292@qq.com>
2017-07-15 22:26:23 +0800

    Core 版本注释

--- zero/DerrickCore/Editions/EditionManager.cs
+++ zero/DerrickCore/Editions/EditionManager.cs
@@ -4,10 +4,21 @@ using Abp.Domain.Repositories;
 
 namespace Derrick.Editions
 {
+    /// <summary>
+    /// <see cref="AbpEditionManager"/>版本实现。版本管理器
+    /// </summary>
     public class EditionManager : AbpEditionManager
     {
+        /// <summary>
+        /// 默认版本名称
+        /// </summary>
         public const string DefaultEditionName = "Standard";
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="editionRepository">版本仓储</param>
+        /// <param name="featureValueStore">功能值存储</param>
         public EditionManager(
             IRepository<Edition> editionRepository, 
             IAbpZeroFeatureValueStore featureValueStore) 
--- zero/DerrickCore/Editions/FeatureValueStore.cs
+++ zero/DerrickCore/Editions/FeatureValueStore.cs
@@ -8,8 +8,20 @@ using Derrick.MultiTenancy;
 
 namespace Derrick.Editions
 {
+    /// <summary>
+    /// 功能值存储
+    /// </summary>
     public class FeatureValueStore : AbpFeatureValueStore<Tenant, User>
     {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="cacheManager">缓存管理</param>
+        /// <param name="tenantFeatureSettingRepository">商户功能设置仓储</param>
+        /// <param name="tenantRepository">商户仓储</param>
+        /// <param name="editionFeatureSettingRepository">版本功能设置仓储</param>
+        /// <param name="featureManager">功能管理</param>
+        /// <param name="unitOfWorkManager">工作单元管理</param>
         public FeatureValueStore(
             ICacheManager cacheManager,
             IRepository<TenantFeatureSetting, long> tenantFeatureSettingRepository,
