32558f0001638ee835d18256773d25cd2952df12
Derrick <550591292@qq.com>
2017-07-15 23:19:01 +0800

    Core 本地化注释

--- zero/DerrickCore/Localization/CultureHelper.cs
+++ zero/DerrickCore/Localization/CultureHelper.cs
@@ -2,8 +2,15 @@
 
 namespace Derrick.Localization
 {
+    /// <summary>
+    /// 区域帮助类
+    /// </summary>
     public static class CultureHelper
     {
+        /// <summary>
+        /// 获取一个值，该值指示当前 System.Globalization.TextInfo 对象是否表示一个文本书写方向为从右到左的书写体系。
+        /// 如果文本书写方向为从右到左，则为 true；否则为 false。
+        /// </summary>
         public static bool IsRtl
         {
             get { return Thread.CurrentThread.CurrentUICulture.TextInfo.IsRightToLeft; }
