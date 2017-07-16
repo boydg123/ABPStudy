024bcbdd8413caae89a7c2d23daea18a75e3fbc0
Derrick <550591292@qq.com>
2017-07-16 14:07:36 +0800

    Core 验证注释

--- zero/DerrickCore/Validation/ValidationHelper.cs
+++ zero/DerrickCore/Validation/ValidationHelper.cs
@@ -3,10 +3,21 @@ using Abp.Extensions;
 
 namespace Derrick.Validation
 {
+    /// <summary>
+    /// 验证帮助类
+    /// </summary>
     public class ValidationHelper
     {
+        /// <summary>
+        /// 邮件正则
+        /// </summary>
         public const string EmailRegex = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
 
+        /// <summary>
+        /// 是否是邮件
+        /// </summary>
+        /// <param name="value">验证的值</param>
+        /// <returns></returns>
         public bool IsEmail(string value)
         {
             if (value.IsNullOrEmpty())
