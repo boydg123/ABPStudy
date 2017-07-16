df1d8e58b79bd7be4e94d5bc1e53c7c72692ed41
Derrick <550591292@qq.com>
2017-07-16 14:10:04 +0800

    Core 站点Url服务注释

--- zero/DerrickCore/Web/IWebUrlService.cs
+++ zero/DerrickCore/Web/IWebUrlService.cs
@@ -1,7 +1,15 @@
 ﻿namespace Derrick.Web
 {
+    /// <summary>
+    /// Web Url服务
+    /// </summary>
     public interface IWebUrlService
     {
+        /// <summary>
+        /// 获取站点根地址
+        /// </summary>
+        /// <param name="tenancyName">商户名称</param>
+        /// <returns></returns>
         string GetSiteRootAddress(string tenancyName = null);
     }
 }
--- zero/DerrickCore/Web/WebUrlService.cs
+++ zero/DerrickCore/Web/WebUrlService.cs
@@ -5,17 +5,35 @@ using Derrick.Configuration;
 
 namespace Derrick.Web
 {
+    /// <summary>
+    /// <see cref="IWebUrlService"/>实现，站点Url服务
+    /// </summary>
     public class WebUrlService : IWebUrlService, ITransientDependency
     {
+        /// <summary>
+        /// 商户名称占位符
+        /// </summary>
         public const string TenancyNamePlaceHolder = "{TENANCY_NAME}";
 
+        /// <summary>
+        /// 设置管理器
+        /// </summary>
         private readonly ISettingManager _settingManager;
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="settingManager">设置管理器</param>
         public WebUrlService(ISettingManager settingManager)
         {
             _settingManager = settingManager;
         }
 
+        /// <summary>
+        /// 获取站点根地址
+        /// </summary>
+        /// <param name="tenancyName">商户名称</param>
+        /// <returns></returns>
         public string GetSiteRootAddress(string tenancyName = null)
         {
             var siteRootFormat = _settingManager.GetSettingValue(AppSettings.General.WebSiteRootAddress).EnsureEndsWith('/');
