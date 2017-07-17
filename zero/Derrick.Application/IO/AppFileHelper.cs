4f06c44ac29c89c254d97f40f2721cbeb3e1fc31
Dou <550591292@qq.com>
2017-07-17 16:13:31 +0800

    应用层文件注释

--- zero/Derrick.Application/IO/AppFileHelper.cs
+++ zero/Derrick.Application/IO/AppFileHelper.cs
@@ -6,8 +6,16 @@ using Abp.IO;
 
 namespace Derrick.IO
 {
+    /// <summary>
+    /// APP文件帮助类
+    /// </summary>
     public static class AppFileHelper
     {
+        /// <summary>
+        /// 读取行
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        /// <returns></returns>
         public static IEnumerable<string> ReadLines(string path)
         {
             using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 0x1000, FileOptions.SequentialScan))
@@ -20,7 +28,11 @@ namespace Derrick.IO
                 }
             }
         }
-
+        /// <summary>
+        /// 删除存在文件夹内的文件
+        /// </summary>
+        /// <param name="folderPath">文件夹路径</param>
+        /// <param name="fileNameWithoutExtension">带扩展名的文件名</param>
         public static void DeleteFilesInFolderIfExists(string folderPath, string fileNameWithoutExtension)
         {
             var directory = new DirectoryInfo(folderPath);
