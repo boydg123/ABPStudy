b4b2b3d779acc82ef8b32b9de6b55e5c5702ff83
Dou <550591292@qq.com>
2017-07-21 18:51:16 +0800

    ABP 模块测试

---
+++ srctest/ABP.Test/Modules/StartupModuleToBeLast_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.Test.Modules
+{
+    /// <summary>
+    /// 启动模块必须在最后
+    /// </summary>
+    public class StartupModuleToBeLast_Tests :TestBaseWithLocalManager
+    {
+
+    }
+}
