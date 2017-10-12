2c5eec637b8c7631e96b65e05631271c65944d9b
Dou <550591292@qq.com>
2017-10-12 16:08:39 +0800

    单元测试

---
+++ srctest/ABP.Test/Notifications/NotificationPublisher_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Abp.BackgroundJobs;
+using Abp.Domain.Uow;
+using Abp.Notifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.Test.Notifications
+{
+    [TestClass]
+    public class NotificationPublisher_Tests : TestBaseWithLocalManager
+    {
+        private readonly NotificationPublisher _publisher;
+        private readonly INotificationStore _store;
+        private readonly IBackgroundJobManager _backgroundJobManager;
+
+        public NotificationPublisher_Tests()
+        {
+            _store = Substitute.For<INotificationStore>();
+            _backgroundJobManager = Substitute.For<IBackgroundJobManager>();
+            _publisher = new NotificationPublisher(_store, _backgroundJobManager, Substitute.For<INotificationDistributer>());
+            _publisher.UnitOfWorkManager = Substitute.For<IUnitOfWorkManager>();
+            _publisher.UnitOfWorkManager.Current.Returns(Substitute.For<IActiveUnitOfWork>());
+        }
+    }
+}
